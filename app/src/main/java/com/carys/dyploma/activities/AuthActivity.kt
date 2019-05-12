1b2228206e586aaf8f3a8a381b31419b01480434
Caroline <cgafiullova@gmail.com>
2019-05-13 00:55:09 +0300

    Add finish method into AuthPresenter to end up AuthActivity after logging in

--- app/src/main/java/com/carys/dyploma/activities/AuthActivity.kt
+++ app/src/main/java/com/carys/dyploma/activities/AuthActivity.kt
@@ -1,26 +1,22 @@
 package com.carys.dyploma.activities
 
+import android.app.Activity
 import android.content.Context
 import android.content.Intent
-import android.content.Intent.FLAG_ACTIVITY_NEW_TASK
 import android.os.Bundle
 import android.text.InputType.TYPE_CLASS_TEXT
 import android.text.InputType.TYPE_TEXT_VARIATION_PASSWORD
 import android.view.Gravity
-import androidx.appcompat.app.AppCompatActivity
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
-import android.widget.LinearLayout
-import androidx.core.content.ContextCompat.startActivity
+import androidx.appcompat.app.AppCompatActivity
 import com.carys.dyploma.R
 import org.jetbrains.anko.*
 import org.jetbrains.anko.sdk27.coroutines.onClick
-import kotlinx.coroutines.*
-import org.json.JSONObject
+
 
 class AuthActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         AuthActivityUi(this.baseContext).setContentView(this)
@@ -71,9 +67,10 @@ class AuthActivity : AppCompatActivity() {
         }
         fun launchActivity() = with(myui) {
             val intent = Intent(ctx, MainViewActivity::class.java)
-            intent.putExtra("id", 5)
-            intent.flags = Intent.FLAG_ACTIVITY_SINGLE_TOP
             ctx.startActivity(intent)
+            (ctx as Activity).finish()
+
+
         }
         fun toast(msg: String) {
             cont.toast(msg)
--- app/src/main/java/com/carys/dyploma/activities/AuthPresenter.kt
+++ app/src/main/java/com/carys/dyploma/activities/AuthPresenter.kt
@@ -41,20 +41,15 @@ class AuthPresenter(val activity: AuthActivity.AuthActivityUi) {
         var parameters: ArrayList<Pair<String, String>> = arrayListOf()
         parameters.add("username" to "pipi")
         parameters.add("password" to "pipipipi")
-        val response = Requester.sendRequest("http://192.168.1.10:8000/api/token/",
+        val response = Requester.sendRequest("https://192.168.1.10:8000/api/token/",
             "POST",  parameters)
         if (response.second != "") {
             val jobj = JSONObject(response.second)
             if (jobj.has("access_token")){
                 val utils = SharedUtils()
                 utils.write("Token", "Bearer " +  jobj.getString("access_token"))
-
             }
         }
         return response.first
     }
-
-
-
 }
-
